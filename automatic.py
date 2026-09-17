51a5b64bc708ddf3bfd3fc61ab1ad48526be574f
unknown <dexter.a.blanchard@gmail.com>
2026-09-17 09:36:41 -0600

    Add main file

--- automatic.py
+++ automatic.py
@@ -26,8 +26,3 @@ def find_sleep_times(times, real_stren):
     longest_sleep = sleeps[sleep_times.index(longest_sleep_time)]
 
     return total_sleep, longest_sleep, longest_sleep_time
-
-times, real_stren = find_total_stren(["0:00", "6:00", "7:00", "7:30", "9:00", "10:00"], ["6:00", "7:00", "7:30", "9:00", "10:00", "12:00"], ["0", "1", "2", "1", "3", "0"])
-print("Times:", times, "Real Stren:", real_stren)
-total_sleep, longest_sleep, longest_sleep_time = find_sleep_times(times, real_stren)
-print("Total Sleep:", total_sleep, "Longest Sleep:", longest_sleep, "Longest Sleep Time:", longest_sleep_time)
